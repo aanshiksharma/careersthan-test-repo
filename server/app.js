7a8a97695ee371f157c7fd32449c8e9b20db552f
Aanshik Sharma <aanshik16@gmail.com>
2024-09-17 01:45:42 +0530

    changes made in app.js in server side for testing of build

--- server/app.js
+++ server/app.js
@@ -1,46 +1,69 @@
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const expressSession = require("express-session");
+// const flash = require("connect-flash");
+// const cookieParser = require("cookie-parser");
+// const PORT = 80;
+
+// require("dotenv").config();
+
+// const db = require("./config/mongoose-connection");
+
+// const indexRouter = require("./routes/index");
+// const usersRouter = require("./routes/usersRouter");
+// const postsRouter = require("./routes/postsRouter");
+// const profileRouter = require("./routes/profileRouter");
+
+// app.use(express.static(path.join(__dirname, "public")));
+// app.set("view engine", "ejs");
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// app.use(
+//   expressSession({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.EXPRESS_SESSION_SECRET,
+//   })
+// );
+// app.use(flash());
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+// app.use("/posts", postsRouter);
+// app.use("/profile", profileRouter);
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).render("index", { error: err.message });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require("express");
-const app = express();
 const path = require("path");
-const expressSession = require("express-session");
-const flash = require("connect-flash");
-const cookieParser = require("cookie-parser");
-const PORT = 80;
 
-require("dotenv").config();
+const app = express();
 
-const db = require("./config/mongoose-connection");
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/usersRouter");
-const postsRouter = require("./routes/postsRouter");
-const profileRouter = require("./routes/profileRouter");
-
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.use(
-  expressSession({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.EXPRESS_SESSION_SECRET,
-  })
-);
-app.use(flash());
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/posts", postsRouter);
-app.use("/profile", profileRouter);
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).render("index", { error: err.message });
+// Your API routes go here
+app.get("/api/someEndpoint", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
 });
 
+// Serve React app for any unmatched routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
